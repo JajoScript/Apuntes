1a6bfdedeed7c83fd22b110be6761bb0dc9297ad
jajoscript <javieralmarzabucarey@gmail.com>
2019-11-08 21:23:55 -0400

    funciones: tipos de parametros en typescript

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-"use strict";
+// string
+var nombre = "Javier";
+// Int
+var numero = 123;
+// Boolean
+var booleano = true;
+// Objeto
+var hoy = new Date();
+// Sin tipo definido
+var cualquiera;
+// Esquemas JSON
+var SpiderMan = {
+    nombre: "Peter",
+    apellido: "Parker",
+    edad: 20
+};
+// Plantillas literales.
+var Nombre = "Javier";
+var Apellido = "Almarza";
+var Edad = 18;
+var Texto = "Hola, " + Nombre + " " + Apellido + " (" + Edad + ") ";
+console.log(Texto);
+// Parametros Obligatorios, por defecto y opcional.
+function activar(quien, // Parametro Obligatorio.
+objeto, // Parametro PorDefecto.
+momento // Parametro Opcional.
+) {
+    if (objeto === void 0) { objeto = "batiseñal"; }
+    var mensaje;
+    if (momento) {
+        mensaje = quien + " activ\u00F3 por defecto la/el " + objeto;
+    }
+    else {
+        mensaje = quien + " activ\u00F3 la/el " + objeto;
+    }
+    console.log(mensaje);
+}
+activar("Javier", "batiseñal", "tarde");
--- app.ts
+++ app.ts
@@ -1,7 +1,51 @@
-var mensaje = "hola";
+// string
+let nombre:string = "Javier";
 
-if(true){
-    var mensaje = "adios";
+// Int
+let numero:number = 123;
+
+// Boolean
+let booleano:boolean = true;
+
+// Objeto
+let hoy:Date = new Date();
+
+// Sin tipo definido
+let cualquiera:any;
+
+// Esquemas JSON
+let SpiderMan = {
+    nombre: "Peter",
+    apellido: "Parker",
+    edad: 20
 }
 
-console.log(mensaje);
+// Plantillas literales.
+let Nombre:string = "Javier";
+let Apellido:string = "Almarza";
+let Edad:number = 18;
+
+let Texto:string = `Hola, ${Nombre} ${Apellido} (${Edad}) `;
+console.log(Texto);
+
+// Parametros Obligatorios, por defecto y opcional.
+
+function activar(   
+    quien:string,                   // Parametro Obligatorio.
+    objeto:string = "batiseñal",    // Parametro PorDefecto.
+    momento?:string                 // Parametro Opcional.
+){
+    
+    let mensaje:string;
+    if(momento){
+        mensaje= `${quien} activó por defecto la/el ${objeto}`;
+    }
+    else{
+        mensaje= `${quien} activó la/el ${objeto}`;
+    }
+    
+
+    console.log(mensaje);
+}
+
+activar("Javier", "batiseñal", "tarde");
